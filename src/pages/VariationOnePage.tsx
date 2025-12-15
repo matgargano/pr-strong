03c28488d8c497b362e6adadf6c62dabf6a62461
matgargano <matgargano@users.noreply.github.com>
2025-12-15 17:41:06 -0500

    update to latest

---
+++ src/pages/VariationOnePage.tsx
@@ -0,0 +1,7 @@
+import VariationOne from "@/components/variations/VariationOne";
+
+const VariationOnePage = () => {
+  return <VariationOne />;
+};
+
+export default VariationOnePage;
---
+++ src/pages/VariationTwoPage.tsx
@@ -0,0 +1,11 @@
+import VariationTwo from "@/components/variations/VariationTwo";
+
+const VariationTwoPage = () => {
+  return (
+    <div className="theme-warm">
+      <VariationTwo />
+    </div>
+  );
+};
+
+export default VariationTwoPage;
